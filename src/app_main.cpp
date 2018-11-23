32be1b0f52f93de021faaa25259b83b837515223
fabriziodesiati <fabrizio.desiati@hotmail.com>
2018-11-23 14:03:41 +0100

    MAJOR: start QProcess python impl

--- src/app_main.cpp
+++ src/app_main.cpp
@@ -62,12 +62,17 @@ int main(int argc,char* argv[])
 {
   /* Load settings */
   QSettings s(HMI_COMPANY, HMI_TITLE);
+  bool bPythonRun = false;
   QString strCfgFilePath = "./etc/BrokerTelegramBot_default.cfg";
   QString strDbFilePath = "./db/BrokerTelegramBot.sqlite";
   QString strTheme;
   QString strAppIdBroker;
   QString strTokenBroker;
   QString strTokenBot;
+  QString strApiId;
+  QString strApiHash;
+  QString strChIdSrc;
+  QString strChIdDst;
   /* Parse arguments */
   QMap<QString,QString*> mapArgs = {
       {"--cfg"          , &strCfgFilePath}
@@ -76,13 +81,19 @@ int main(int argc,char* argv[])
     , {"--app_id-broker", &strAppIdBroker}
     , {"--token-broker" , &strTokenBroker}
     , {"--token-bot"    , &strTokenBot   }
-    
+    , {"--api_id"       , &strApiId      }
+    , {"--api_hash"     , &strApiHash    }
+    , {"--ch_id_src"    , &strChIdSrc    }
+    , {"--ch_id_dst"    , &strChIdDst    }
   };
   for (auto j = 0; j < argc; ++j) {
     QString strArgName = argv[j];
     if      ("--clear" == strArgName) {
       /* Clear settings */
       s.clear();
+    }
+    else if ("--python" == strArgName) {
+      bPythonRun = true;
     }
     else if (mapArgs.contains(strArgName)) {
       const char* pStr = argv[++j];
@@ -102,6 +113,10 @@ int main(int argc,char* argv[])
       , {"app_id-broker", &strAppIdBroker }
       , {"token-broker" , &strTokenBroker }
       , {"token-bot"    , &strTokenBot    }
+      , {"api_id"       , &strApiId       }
+      , {"api_hash"     , &strApiHash     }
+      , {"ch_id_src"    , &strChIdSrc     }
+      , {"ch_id_dst"    , &strChIdDst     }
     };
     for(auto strKeyName: mapCfg.keys()) {
       QString* pKeyValue = mapCfg.value(strKeyName);
@@ -111,6 +126,14 @@ int main(int argc,char* argv[])
   CATCH_ABORT(strAppIdBroker.isEmpty(), "Required --app_id-broker <APP_ID>");
   CATCH_ABORT(strTokenBroker.isEmpty(), "Required --token-broker <TOKEN>");
   CATCH_ABORT(strTokenBot.isEmpty()   , "Required --token-bot <TOKEN>");
+  CATCH_ABORT(bPythonRun && strApiId.isEmpty()
+    , "Required --api_id <API_ID>");
+  CATCH_ABORT(bPythonRun && strApiHash.isEmpty()
+    , "Required --api_hash <API_HASH>");
+  CATCH_ABORT(bPythonRun && strChIdSrc.isEmpty()
+    , "Required --ch_id_src <CHANNEL_ID_SOURCE>");
+  CATCH_ABORT(bPythonRun && strChIdDst.isEmpty()
+    , "Required --ch_id_dst <CHANNEL_ID_DESTINATION>");
 
   /* Create Application */
   // Set important Qt flags
